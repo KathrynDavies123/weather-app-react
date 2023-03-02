71da9c2053945988fb64809cde925d82dec24bc8
“KathrynDavies123” <kathryn.davies1233@gmail.com>
2023-03-02 11:12:30 +0100

    indenting and tidying

--- src/components/sections/headersection.js
+++ src/components/sections/headersection.js
@@ -3,27 +3,32 @@ import { useEffect, useState } from "react";
 import Moment from "react-moment";
 
 const HeaderSection = () => {
-   const [weather, setWeather] = useState([]);
-   const [temperature, setTemperature] = useState([]);
-   const [location, setLocation] = useState([]);
-   useEffect(() => {
-       fetch(`https://api.openweathermap.org/data/2.5/weather?lat=52.468868477131615&lon=13.3898461&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
-         .then((response) => response.json())
-         .then((data) => {
-            setWeather(data.weather[0]);
-            setTemperature(data.main);
-            setLocation(data.name);
-        });
-     }, []);
+  const [weather, setWeather] = useState([]);
+  const [temperature, setTemperature] = useState([]);
+  const [location, setLocation] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=52.468868477131615&lon=13.3898461&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setWeather(data.weather[0]);
+        setTemperature(data.main);
+        setLocation(data.name);
+      });
+  }, []);
 
   const todaysdate = new Date();
-  let currenttemp = Math.round(temperature.temp); 
-  
+  let currenttemp = Math.round(temperature.temp);
+
   return (
     <header>
       <Moment format="dddd Do MMMM">{todaysdate}</Moment>
       <p>{`${weather.description}`}</p>
-      <img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt=''/>
+      <img
+        src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+        alt=""
+      />
       <p>{`${currenttemp} degrees celcius`}</p>
       <p>{`${location}`}</p>
     </header>
--- src/components/tiles/detailstile.js
+++ src/components/tiles/detailstile.js
@@ -1,10 +1,14 @@
 import "./detailstile.css";
 
 const DetailsTile = (props) => {
-    return ( <div>
-        <h3>{props.title}</h3>
-        <p>{props.measurement} {props.secondary}</p>
-    </div> );
-}
- 
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <p>
+        {props.measurement} {props.secondary}
+      </p>
+    </div>
+  );
+};
+
 export default DetailsTile;
